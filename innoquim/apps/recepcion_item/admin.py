096e278d4c8354894d7d10d3bdfa42d09cdab09d
Jean Alexander Cedeño Saltos <e1314689066@live.uleam.edu.ec>
2025-10-26 18:47:22 -0500

    feat: Implementación de API REST para Recepción Item

--- innoquim/apps/recepcion_item/admin.py
+++ innoquim/apps/recepcion_item/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import RecepcionItem
 
-# Register your models here.
+@admin.register(RecepcionItem)
+class RecepcionItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'id_recepcion_material', 'cantidad', 'id_unidad', 'lote')
+    list_filter = ('id_recepcion_material', 'id_unidad', 'lote')
+    search_fields = ('lote', 'observaciones', 'id_recepcion_material__observaciones')
+    ordering = ('-id_recepcion_material__fecha_recepcion', 'lote')
