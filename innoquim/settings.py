bbb654270125d6a7ff75347bff0c24039afb2137
Nathaly Holguin Ordoñez <e1725190134@live.uleam.edu.ec>
2026-01-15 20:47:16 -0500

    Add default SECRET_KEY for collectstatic

--- innoquim/settings.py
+++ innoquim/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fake-key-for-build-only-do-not-use-in-production")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
